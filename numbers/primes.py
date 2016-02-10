4621fa507ff33a5af3453ab59c880ea7a43c12b7
michsoch <msoch@seas.upenn.edu>
2016-02-09 21:37:00 -0500

    add documentation to primes file

--- numbers/primes.py
+++ numbers/primes.py
@@ -1,15 +1,13 @@
 from customexceptions import custom_exceptions
 
-
-# TODO: different functionality for calculating allowed small vs suitably large p?
-# TODO: for now, primes code based on https://github.com/blockstack/secret-sharing/blob/master/secretsharing/primes.py
 mersenne_prime_exponents = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279,
                             2203, 2281, 3217, 4253, 4423]
 
 
 def mersenne_primes():
     '''
-    Returns all the mersenne primes with less than 2000 digits.
+    Returns:
+        all the mersenne primes with less than 5000 digits.
     '''
     primes = []
 
@@ -19,17 +17,29 @@ def mersenne_primes():
     return primes
 
 
-def get_prime_by_bitlength(secret_size):
-    if (secret_size < 0):
+def get_prime_by_bitlength(size):
+    '''
+    Returns:
+        a prime with strictly more bits than the specified size
+        or None if no such prime is found in hardcoded list
+    Raises:
+        IllegalArgumentException, size is negative
+    '''
+    if (size < 0):
         raise custom_exceptions.IllegalArgumentException
 
     for exp in mersenne_prime_exponents:
-        if exp > secret_size:
+        if exp > size:
             return 2**exp - 1
     return None
 
 
 def get_prime_by_batch(batch):
+    '''
+    Returns:
+        a prime strictly larger than all values in the list provided in batch
+        or None if no such prime is found in hardcoded list
+    '''
     max_item = max(batch)
     primes = mersenne_primes()
 
